441997d8421be03c6860ac67b77695753cc066f5
nbulgarides42 <104031311+nbulgarides42@users.noreply.github.com>
2025-02-01 00:36:49 -0500

    X

--- js/initialization/chadUtilities.js
+++ js/initialization/chadUtilities.js
@@ -1,5 +1,6 @@
 class ChadUtilities {
   static describeVector(msg, vector) {
+    return;
     console.log(
       "MSG: " +
         msg +
@@ -12,6 +13,7 @@ class ChadUtilities {
     );
   }
   static describePositionedObject(msg, object) {
+    return;
     console.log(
       "MSG: " +
         msg +
